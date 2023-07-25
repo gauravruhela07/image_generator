5d4fc68282002e234727bb7aef52443bb6dc93df
Gaurav Ruhela <gauravruhela@MacBook-Pro-7.local>
2023-07-25 07:51:37 +0400

    image card on homepage functionality

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -18,6 +18,30 @@ const Home = () => {
 
     const [searchText, setSearchText] = useState('');
 
+    useEffect(() => {
+      const fetchPosts = async () => {
+        setLoading(true);
+
+        try {
+          const response = await fetch('http://localhost:8080/api/v1/posts', {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },            
+          });
+          if(response.ok) {
+            const result = await response.json();            
+            setAllPosts(result.data.reverse());
+          }
+        } catch (error) {
+          alert(error);
+        } finally {
+          setLoading(false);
+        }
+      }
+      fetchPosts();
+    }, []);
+
   return (
     <section className="max-w-7xl mx-auto">
         <div> 
@@ -31,7 +55,7 @@ const Home = () => {
 
         <div className="mt-10">
           {loading ? (
-            <div clasName="flex justify-center item-center">
+            <div className="flex justify-center item-center">
               <Loader />
             </div>
           ) : (
@@ -44,12 +68,12 @@ const Home = () => {
               <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
                 {searchText ? (
                   <RenderCards 
-                    data={[]}
+                    data={allPosts}
                     title="No search results found"
                   />
                 ) : (
                   <RenderCards 
-                    data={[]}
+                    data={allPosts}
                     title="No posts found"
                   />
                 )}
